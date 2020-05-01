0c0306623383d18e31d218d701333a8a09f9d3e3
OpenUEyes <romanenkoroman.dev@gmail.com>
2020-05-01 15:11:34 +0300

    add db init script

--- src/main/java/com/company/external/database/db_init.sql
+++ src/main/java/com/company/external/database/db_init.sql
@@ -1,117 +1,100 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Win32 (AMD64)
---
--- Host: localhost    Database: bank
--- ------------------------------------------------------
--- Server version	8.0.19
+﻿/* SQL Manager Lite for MySQL                              5.8.0.53447 */
+/* ------------------------------------------------------------------- */
+/* Host     : localhost                                                */
+/* Port     : 3306                                                     */
+/* Database : bank                                                     */
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES 'utf8mb4' */;
 
---
--- Current Database: `bank`
---
+SET FOREIGN_KEY_CHECKS=0;
 
-/*!40000 DROP DATABASE IF EXISTS `bank`*/;
+DROP DATABASE IF EXISTS `bank`;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `bank` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `bank`
+    CHARACTER SET 'utf8mb4'
+    COLLATE 'utf8mb4_0900_ai_ci';
 
 USE `bank`;
 
---
--- Table structure for table `account`
---
+SET sql_mode = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION';
 
-DROP TABLE IF EXISTS `account`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `account` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `login` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci PACK_KEYS=0;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `bill`
---
-
-DROP TABLE IF EXISTS `bill`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bill` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `type` enum('DEPOSIT','CREDIT') NOT NULL,
-  `balance` decimal(10,0) NOT NULL,
-  `validity` date DEFAULT NULL,
-  `account_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `bill_fk1` (`account_id`),
-  CONSTRAINT `bill_fk1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci PACK_KEYS=0;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `credit`
---
-
-DROP TABLE IF EXISTS `credit`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `credit` (
-  `id` int NOT NULL,
-  `debt` decimal(10,0) NOT NULL,
-  `limit` decimal(10,0) NOT NULL,
-  `percentage` decimal(10,0) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  CONSTRAINT `credit_fk1` FOREIGN KEY (`id`) REFERENCES `bill` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci PACK_KEYS=0;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `deposit`
---
+/* Dropping database objects */
 
 DROP TABLE IF EXISTS `deposit`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+DROP TABLE IF EXISTS `credit`;
+DROP TABLE IF EXISTS `bill`;
+DROP TABLE IF EXISTS `account`;
+
+/* Structure for the `account` table : */
+
+CREATE TABLE `account` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(30) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` VARCHAR(30) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` VARCHAR(40) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` VARCHAR(30) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `surname` VARCHAR(30) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `phone_number` VARCHAR(30) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY USING BTREE (`id`),
+  UNIQUE KEY `id` USING BTREE (`id`),
+  UNIQUE KEY `login` USING BTREE (`login`),
+  UNIQUE KEY `email` USING BTREE (`email`),
+  UNIQUE KEY `phone_number` USING BTREE (`phone_number`)
+) ENGINE=InnoDB
+AUTO_INCREMENT=12 ROW_FORMAT=DYNAMIC CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci'
+;
+
+/* Structure for the `bill` table : */
+
+CREATE TABLE `bill` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `type` ENUM('DEPOSIT','CREDIT','UNSIGNED') COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `balance` FLOAT NOT NULL,
+  `validity` DATE DEFAULT NULL,
+  `account_id` INTEGER NOT NULL,
+  PRIMARY KEY USING BTREE (`id`),
+  UNIQUE KEY `id` USING BTREE (`id`),
+  KEY `bill_fk1` USING BTREE (`account_id`),
+  CONSTRAINT `bill_fk1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB
+AUTO_INCREMENT=46 ROW_FORMAT=DYNAMIC CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci'
+;
+
+/* Structure for the `credit` table : */
+
+CREATE TABLE `credit` (
+  `id` INTEGER NOT NULL,
+  `debt` FLOAT NOT NULL,
+  `limit` FLOAT NOT NULL,
+  `percentage` FLOAT NOT NULL,
+  `start` DATE NOT NULL,
+  `deadline` DATE NOT NULL,
+  PRIMARY KEY USING BTREE (`id`),
+  UNIQUE KEY `id` USING BTREE (`id`),
+  CONSTRAINT `credit_fk1` FOREIGN KEY (`id`) REFERENCES `bill` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB
+ROW_FORMAT=DYNAMIC CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci'
+;
+
+/* Structure for the `deposit` table : */
+
 CREATE TABLE `deposit` (
-  `id` int NOT NULL,
-  `amount` decimal(10,0) NOT NULL,
-  `rate` decimal(10,0) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
+  `id` INTEGER NOT NULL,
+  `amount` FLOAT NOT NULL,
+  `rate` FLOAT NOT NULL,
+  `start` DATE NOT NULL,
+  `finish` DATE NOT NULL,
+  PRIMARY KEY USING BTREE (`id`),
+  UNIQUE KEY `id` USING BTREE (`id`),
   CONSTRAINT `deposit_fk1` FOREIGN KEY (`id`) REFERENCES `bill` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci PACK_KEYS=0;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB
+ROW_FORMAT=DYNAMIC CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci'
+;
 
---
--- Dumping events for database 'bank'
---
-
---
--- Dumping routines for database 'bank'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-03-18 17:18:40
